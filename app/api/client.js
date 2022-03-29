b938c3e7fb6b1b9729dafdcee7cdbdd58c0a9e63
peelu123 <raghuwanshipalash6@gmail.com>
2022-03-29 12:41:48 +0530

    IP

--- app/api/client.js
+++ app/api/client.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const client = axios.create({baseURL: 'http://192.168.29.161:2121/api'})
+const client = axios.create({baseURL: 'http://your_IP:2121/api'})
 
 export default client;
